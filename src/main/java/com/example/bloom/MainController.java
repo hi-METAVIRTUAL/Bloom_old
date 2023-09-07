ab4627b571f88ff90ae36d7890393a134d6509ae
wjs960 <wjs960@gmail.com>
2023-09-07 17:05:32 +0900

    2023-09-07 sj 검사 시작 페이지

---
+++ src/main/java/com/example/bloom/MainController.java
@@ -0,0 +1,28 @@
+package com.example.bloom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public void gomain() {
+    }
+
+    @GetMapping("/")
+    public String defaultLocation() {
+        return "index";
+    }
+
+    @GetMapping("test")
+    public String test(){
+
+        return "psychological/firstTest";
+    }
+    @GetMapping("start")
+    public String test1(){
+
+        return "psychological/testStart";
+    }
+}
